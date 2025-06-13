d9b8ac20c0ba486a9677f42debbd7f1ab3337568
aviraj834051 <rajnishkumar834051@gmail.com>
2025-06-13 12:31:38 +0530

    Update token_extractor.py

--- token_extractor.py
+++ token_extractor.py
@@ -1,21 +1,20 @@
 import requests
-import re
 
-def extract_token(cookies_string):
-    cookies = {}
-    for item in cookies_string.split(";"):
-        key, value = item.strip().split("=", 1)
-        cookies[key] = value
-
+def get_token_from_cookies(cookie):
     headers = {
+        "Host": "business.facebook.com",
         "User-Agent": "Mozilla/5.0 (Linux; Android 10)",
-        "Accept-Language": "en-US,en;q=0.9"
+        "Accept": "*/*",
+        "Cookie": cookie
     }
 
-    response = requests.get("https://business.facebook.com/business_locations", headers=headers, cookies=cookies)
-    access_token = re.search(r'EAAG\w+', response.text)
-
-    if access_token:
-        return access_token.group(0)
-    else:
-        raise Exception("Valid group-messaging token not found. Check your Facebook account settings.")
+    try:
+        response = requests.get("https://business.facebook.com/business_locations", headers=headers)
+        if "EAAG" in response.text:
+            token = response.text.split('["EAAG')[1].split('"')[0]
+            return "EAAG" + token
+        else:
+            return None
+    except Exception as e:
+        print(f"Error while extracting token: {e}")
+        return None
